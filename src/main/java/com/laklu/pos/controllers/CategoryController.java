0d204643da964c291a261df2a5efaf84f7a09da8
Quynhnvhe163568 <quynhnvhe163568@fpt.edu.vn>
2025-02-19 14:45:19 +0700

    Hot Fix: Fix format code follow recommend Git

--- src/main/java/com/laklu/pos/controllers/CategoryController.java
+++ src/main/java/com/laklu/pos/controllers/CategoryController.java
@@ -5,42 +5,44 @@ import com.laklu.pos.auth.policies.CategoryPolicy;
 import com.laklu.pos.dataObjects.ApiResponseEntity;
 import com.laklu.pos.dataObjects.request.CategoryRequest;
 import com.laklu.pos.dataObjects.response.CategoryResponse;
+import com.laklu.pos.entities.Category;
 import com.laklu.pos.exceptions.httpExceptions.ForbiddenException;
+import com.laklu.pos.mapper.CategoryMapper;
 import com.laklu.pos.services.CategoryService;
 import com.laklu.pos.uiltis.Ultis;
 import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/categories")
+@AllArgsConstructor
 public class CategoryController {
 
-    @Autowired
-    private CategoryPolicy categoryPolicy;
-
-    @Autowired
-    private CategoryService categoryService;
+    private final CategoryPolicy categoryPolicy;
+    private final CategoryService categoryService;
+    private final CategoryMapper categoryMapper;
 
     // Lấy danh sách tất cả categories
     @GetMapping
     public ApiResponseEntity getAllCategories() throws Exception{
         Ultis.throwUnless(categoryPolicy.canList(JwtGuard.userPrincipal()), new ForbiddenException());
-        List<CategoryResponse> categories = categoryService.getAllCategories();
-        return ApiResponseEntity.success(categories, "Lấy danh sách categories thành công");
+        List<Category> categories = categoryService.getAllCategories();
+        List<CategoryResponse> categoryResponses = categoryMapper.toResponseList(categories);
+        return ApiResponseEntity.success(categoryResponses, "Lấy danh sách categories thành công");
     }
 
     // Lấy thông tin một category theo ID
     @GetMapping("/{id}")
     public ApiResponseEntity getCategoryById(@PathVariable Long id) throws Exception{
-        var category = categoryService.findOrFail(id);
-        Ultis.throwUnless(categoryPolicy.canView(JwtGuard.userPrincipal(), category), new ForbiddenException());
-        CategoryResponse categoryResponse = categoryService.getCategoryById(id);
-        if (category == null) {
-            return ApiResponseEntity.exception(HttpStatus.NOT_FOUND, "Không tìm thấy category với ID: " + id);
-        }
+        var categoryExist = categoryService.findOrFail(id);
+        Ultis.throwUnless(categoryPolicy.canView(JwtGuard.userPrincipal(), categoryExist), new ForbiddenException());
+        Category category = categoryService.getCategoryById(id);
+        CategoryResponse categoryResponse = categoryMapper.toResponse(category);
         return ApiResponseEntity.success(categoryResponse, "Lấy thông tin category thành công");
     }
 
@@ -48,21 +50,21 @@ public class CategoryController {
     @PostMapping
     public ApiResponseEntity createCategory( @Valid @RequestBody CategoryRequest categoryRequest)throws Exception{
         Ultis.throwUnless(categoryPolicy.canCreate(JwtGuard.userPrincipal()), new ForbiddenException());
-        CategoryResponse createdCategory = categoryService.createCategory(categoryRequest);
+        Category category = categoryService.createCategory(categoryRequest);
+        CategoryResponse createdCategory = categoryMapper.toResponse(category);
         return ApiResponseEntity.success(createdCategory, "Tạo category thành công");
     }
 
     // Cập nhật category
-    @PutMapping("/{id}")
-    public ApiResponseEntity updateCategory(@PathVariable Long id, @RequestBody CategoryRequest categoryRequest) throws Exception{
-        var  category = categoryService.findOrFail(id);
-        Ultis.throwUnless(categoryPolicy.canEdit(JwtGuard.userPrincipal(), category), new ForbiddenException());
-        CategoryResponse updatedCategory = categoryService.updateCategory(id, categoryRequest);
-        if (updatedCategory == null) {
-            return ApiResponseEntity.exception(HttpStatus.NOT_FOUND, "Không tìm thấy category với ID: " + id);
-        }
+    @PatchMapping("/{id}")
+    public ApiResponseEntity updateCategoryPartially(@PathVariable Long id, @RequestBody Map<String, Object> updates) throws Exception {
+        var categoryExist = categoryService.findOrFail(id);
+        Ultis.throwUnless(categoryPolicy.canEdit(JwtGuard.userPrincipal(), categoryExist), new ForbiddenException());
+        Category category = categoryService.updateCategoryPartially(id, updates);
+        CategoryResponse updatedCategory = categoryMapper.toResponse(category);
         return ApiResponseEntity.success(updatedCategory, "Cập nhật category thành công");
     }
+
 
     // Xóa mềm category (Soft Delete)
     @DeleteMapping("/{id}")
--- src/main/java/com/laklu/pos/services/CategoryService.java
+++ src/main/java/com/laklu/pos/services/CategoryService.java
@@ -5,37 +5,40 @@ import com.laklu.pos.dataObjects.response.CategoryResponse;
 import com.laklu.pos.entities.Category;
 import com.laklu.pos.exceptions.CategoryAlreadyExistsException;
 import com.laklu.pos.exceptions.NotFoundCategory;
+import com.laklu.pos.exceptions.RestHttpException;
 import com.laklu.pos.mapper.CategoryMapper;
 import com.laklu.pos.repositories.CategoryRepository;
 import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
-
+@AllArgsConstructor
 @Service
 public class CategoryService {
-    @Autowired
-    private CategoryRepository categoryRepository;
-    @Autowired
-    private CategoryMapper categoryMapper;
+
+    private final CategoryRepository categoryRepository;
+    private final CategoryMapper categoryMapper;
     // Lấy danh sách tất cả Categories
     @Transactional
-    public List<CategoryResponse> getAllCategories() {
+    public List<Category> getAllCategories() {
         List<Category> categories = categoryRepository.findAll();
-        return categoryMapper.toResponseList(categories);
+        return categories;
     }
 
     // Lấy một Category theo ID
-    public CategoryResponse getCategoryById(Long id) {
+    public Category getCategoryById(Long id) {
         Category category = categoryRepository.findById(id).orElseThrow(() -> new NotFoundCategory());
-        return categoryMapper.toResponse(category);
+        return category;
     }
 
     // Thêm mới Category
     @Transactional
-    public CategoryResponse createCategory(CategoryRequest categoryRequest) {
+    public Category createCategory(CategoryRequest categoryRequest) {
        categoryRepository.findByName(categoryRequest.getName()).ifPresent(existingCategory -> {
             throw new CategoryAlreadyExistsException();
         });
@@ -44,21 +47,7 @@ public class CategoryService {
         category.setCreatedAt(new java.util.Date());
         category.setUpdatedAt(new java.util.Date());
         category.setIsDeleted(false);
-        return categoryMapper.toResponse(categoryRepository.save(category));
-    }
-
-    // Cập nhật Category
-    @Transactional
-    public CategoryResponse updateCategory(Long id, CategoryRequest categoryRequest) {
-        Optional<Category> optionalCategory = categoryRepository.findById(id);
-        if (optionalCategory.isPresent()) {
-            Category category = optionalCategory.get();
-            category.setName(categoryRequest.getName());
-            category.setDescription(categoryRequest.getDescription());
-            category.setUpdatedAt(new java.util.Date());
-            return categoryMapper.toResponse(categoryRepository.save(category));
-        }
-        return null;
+        return categoryRepository.save(category);
     }
 
     // Xóa mềm (Soft Delete)
@@ -77,5 +66,20 @@ public class CategoryService {
     public Category findOrFail(Long id) {
         return this. findByCategoryById(id).orElseThrow(()
                 -> new NotFoundCategory());
+    }
+
+    @Transactional
+    public Category updateCategoryPartially(Long id, Map<String, Object> updates) {
+        Category category = categoryRepository.findById(id)
+                .orElseThrow(() -> new NotFoundCategory());
+        updates.forEach((key, value) -> {
+            switch (key) {
+                case "name" -> category.setName((String) value);
+                case "description" -> category.setDescription((String) value);
+            }
+        });
+
+        category.setUpdatedAt(new Date());
+        return categoryRepository.save(category);
     }
 }
