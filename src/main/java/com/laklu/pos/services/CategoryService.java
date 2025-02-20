72fbce7088c4b0d371765ecf0b0eb368876c8025
VuLinh1 <133078733+VuLinh1@users.noreply.github.com>
2025-02-20 19:17:28 +0700

    chore: category crud
    * chore: category crud
    
    * feat: update category update method to accept partial updates
    
    ---------
    
    Co-authored-by: vktrung <trung040102@gmail.com>

--- src/main/java/com/laklu/pos/services/CategoryService.java
+++ src/main/java/com/laklu/pos/services/CategoryService.java
@@ -1,77 +1,84 @@
 package com.laklu.pos.services;
 
 import com.laklu.pos.dataObjects.request.CategoryRequest;
-import com.laklu.pos.dataObjects.response.CategoryResponse;
 import com.laklu.pos.entities.Category;
-import com.laklu.pos.exceptions.CategoryAlreadyExistsException;
 import com.laklu.pos.exceptions.NotFoundCategory;
-import com.laklu.pos.exceptions.RestHttpException;
 import com.laklu.pos.mapper.CategoryMapper;
 import com.laklu.pos.repositories.CategoryRepository;
+import com.laklu.pos.validator.RuleValidator;
+import com.laklu.pos.validator.ValueExistIn;
 import jakarta.transaction.Transactional;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-@AllArgsConstructor
+
 @Service
+@AllArgsConstructor
 public class CategoryService {
 
     private final CategoryRepository categoryRepository;
+
     private final CategoryMapper categoryMapper;
-    // Lấy danh sách tất cả Categories
-    @Transactional
+
     public List<Category> getAllCategories() {
-        List<Category> categories = categoryRepository.findAll();
-        return categories;
+        return categoryRepository.findAll();
     }
 
-    // Lấy một Category theo ID
-    public Category getCategoryById(Long id) {
-        Category category = categoryRepository.findById(id).orElseThrow(() -> new NotFoundCategory());
-        return category;
+    public List<Category> getAllCategoriesDeleted() {
+        return categoryRepository.findAllByIsDeletedFalse();
     }
 
-    // Thêm mới Category
-    @Transactional
     public Category createCategory(CategoryRequest categoryRequest) {
-       categoryRepository.findByName(categoryRequest.getName()).ifPresent(existingCategory -> {
-            throw new CategoryAlreadyExistsException();
-        });
+        ValueExistIn<String> valueExistIn = new ValueExistIn<>(
+                "name",
+                categoryRequest.getName(),
+                (name) -> categoryRepository.findByName(name).isEmpty()
+        );
+        valueExistIn.setMessage("Danh mục đã tồn tại");
+        RuleValidator.validate(valueExistIn);
 
         Category category = categoryMapper.toEntity(categoryRequest);
         category.setCreatedAt(new java.util.Date());
         category.setUpdatedAt(new java.util.Date());
         category.setIsDeleted(false);
+
         return categoryRepository.save(category);
     }
 
-    // Xóa mềm (Soft Delete)
     @Transactional
-    public void deleteCategory(Long id) {
-        Optional<Category> optionalCategory = categoryRepository.findById(id);
-        optionalCategory.ifPresent(category -> {
-            category.setIsDeleted(true);
-            categoryRepository.save(category);
-        });
+    public Category updateCategory(Category category, CategoryRequest categoryRequest) {
+        ValueExistIn<String> valueExistIn = new ValueExistIn<>(
+                "name",
+                categoryRequest.getName(),
+                (name) -> categoryRepository.findByName(name).isEmpty()
+        );
+        valueExistIn.setMessage("Danh mục đã tồn tại");
+        RuleValidator.validate(valueExistIn);
+
+        category.setName(categoryRequest.getName());
+        category.setDescription(categoryRequest.getDescription());
+        category.setUpdatedAt(new java.util.Date());
+
+        return category;
     }
 
-    public Optional<Category> findByCategoryById(Long id) {
-        return categoryRepository.findById(id);
+    @Transactional
+    public void deleteCategory(Category category) {
+        category.setIsDeleted(true);
+        categoryRepository.save(category);
     }
+
+
     public Category findOrFail(Long id) {
-        return this. findByCategoryById(id).orElseThrow(()
-                -> new NotFoundCategory());
+        return this.categoryRepository.findById(id).orElseThrow(NotFoundCategory::new);
     }
 
     @Transactional
-    public Category updateCategoryPartially(Long id, Map<String, Object> updates) {
-        Category category = categoryRepository.findById(id)
-                .orElseThrow(() -> new NotFoundCategory());
+    public Category updateCategoryPartially(Category category, Map<String, Object> updates) {
         updates.forEach((key, value) -> {
             switch (key) {
                 case "name" -> category.setName((String) value);
