950bfb6a989986fe81b44e04261381a32f4ff4a3
vktrung <trung040102@gmail.com>
2025-03-01 21:47:25 +0700

    refactor: enhance DishResponse to include images in constructor and use Lombok annotations

--- src/main/java/com/laklu/pos/dataObjects/response/DishResponse.java
+++ src/main/java/com/laklu/pos/dataObjects/response/DishResponse.java
@@ -3,11 +3,15 @@ package com.laklu.pos.dataObjects.response;
 import com.laklu.pos.entities.Dish;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @Builder
 public class DishResponse {
     private Integer id;
@@ -18,13 +22,14 @@ public class DishResponse {
     private Double price;
     private List<PersistAttachmentResponse> images;
 
-    public DishResponse(Integer id, String name, String description, LocalDateTime createdAt, LocalDateTime updatedAt, Double price) {
+    public DishResponse(Integer id, String name, String description, LocalDateTime createdAt, LocalDateTime updatedAt, Double price, List<PersistAttachmentResponse> images) {
         this.id = id;
         this.name = name;
         this.description = description;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
         this.price = price;
+        this.images = images;
     }
 
     public static DishResponse fromEntity(Dish dish) {
@@ -34,7 +39,8 @@ public class DishResponse {
                 dish.getDescription(),
                 dish.getCreatedAt() ,
                 dish.getUpdatedAt(),
-                dish.getPrice()
+                dish.getPrice(),
+                new ArrayList<>()
         );
     }
 }
